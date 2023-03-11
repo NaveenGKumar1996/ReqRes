b46bfb032bf56335f8d59843edd062064dddfc5b
naveenG1996 <gnaveenkumar9121996@gmail.com>
2023-03-11 14:18:26 +0530

    Initial API commit

---
+++ src/test/java/com/reqres/utils/EmployeeDataTest.java
@@ -0,0 +1,11 @@
+package com.reqres.utils;
+
+import org.testng.annotations.Test;
+
+public class EmployeeDataTest {
+
+    @Test
+    public void employeePostiveCase() {
+        APIUtils.postRequest();
+    }
+}
